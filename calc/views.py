7f2eabda6b27b7d4b5a6e71b7679299b694c1426
Lucia Villa Martinez <lucia.villa.martinez@gmail.com>
2016-03-23 10:02:01 +0100

    modificacion del fichero calc/views

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def suma(request,num1,num2):
+	resultado = int(num1) + int(num2)
+	return HttpResponse('El resultado de su suma es:' + str(resultado))
+
+def resta(request,num1,num2):
+	resultado = int(num1) - int(num2)
+	return HttpResponse('El resultado de su resta es:' + str(resultado))
+
+def multiplicacion(request,num1,num2):
+	resultado = int(num1) * int(num2)
+	return HttpResponse('El resultado de su multiplicacion es:' + str(resultado))
+
+def division(request,num1,num2):
+	resultado = int(num1) / int(num2)
+	return HttpResponse('El resultado de su division es:' + str(resultado))
+
+def fallo(request):
+	return HttpResponse('<h1><font color="red"> Not Found!</font></h1>')
+
